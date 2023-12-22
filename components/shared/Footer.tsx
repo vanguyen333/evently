3baed0f2edda37c849492d3475ee6418ead30969
vanguyen333 <vanguyen333@yahoo.com>
2023-12-22 16:01:49 -0500

    added logo and border to footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div>
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
