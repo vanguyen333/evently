f952ccc8ff8e31b28c394ed5ee8e39ea6c4bb6ce
vanguyen333 <vanguyen333@yahoo.com>
2023-12-22 15:28:57 -0500

    added unstlyled items to sidenav

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,7 +1,19 @@
+import { headerLinks } from "@/constants";
+import Link from "next/link";
 import React from "react";
 
 const NavItems = () => {
-  return <ul className="md:flex-between flex"></ul>;
+  return (
+    <ul className="md:flex-between flex">
+      {headerLinks.map((link) => {
+        return (
+          <li>
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default NavItems;
