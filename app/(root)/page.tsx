a0418406722df2150b9a86788c297b7f30ea19c8
vanguyen333 <vanguyen333@yahoo.com>
2023-12-22 16:12:55 -0500

    added h1 to hero with styling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,5 +2,17 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cotain py-5 md:py-10">
+        <div className="wrapper grid-cols-1 gap-1 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Host, Connect, Celebrate: Your Events, Our Platform!
+            </h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
