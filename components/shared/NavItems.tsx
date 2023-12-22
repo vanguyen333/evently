ab9cb6e0c593588dce50ac9fca078cd7f4963f88
vanguyen333 <vanguyen333@yahoo.com>
2023-12-22 15:31:18 -0500

    added nicely spacing and postion to nav items

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const NavItems = () => {
   return (
-    <ul className="md:flex-between flex">
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
       {headerLinks.map((link) => {
         return (
           <li>
