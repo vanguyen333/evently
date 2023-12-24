567de36a71420a4a482fe71851646663d3fa1a2f
vanguyen333 <vanguyen333@yahoo.com>
2023-12-24 16:29:43 -0500

    set up basic connection with mongodb

---
+++ lib/database/index.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+};
