92fd6ce313820effe95d2075e65026df941dc1e6
vanguyen333 <vanguyen333@yahoo.com>
2023-12-27 22:51:16 -0500

    added form template

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -40,7 +40,29 @@ const EventForm = ({ userId, type }: EventFormProps) => {
     console.log(values);
   }
 
-  return <div>EventForm {type}</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default EventForm;
