2fc909e2e2f5d35dce30a1f5899f78f769ca02d4
vanguyen333 <vanguyen333@yahoo.com>
2023-12-27 22:41:38 -0500

    create formSchema

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 type EventFormProps = {
   userId: string;
   type: "Create" | "Update";
