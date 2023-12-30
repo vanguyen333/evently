ef31499989db0a00f9da024c7a3d2bbda0cc3c22
vanguyen333 <vanguyen333@yahoo.com>
2023-12-30 11:14:16 -0500

    added code from https://docs.uploadthing.com/api-reference/react#usedropzone

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -5,6 +5,57 @@ type FileUploadProps = {
   onFieldChange: (value: string) => void;
   setFiles: Dispatch<SetStateAction<File[]>>;
 };
+// Note: `useUploadThing` is IMPORTED FROM YOUR CODEBASE using the `generateReactHelpers` function
+import type { FileWithPath } from "@uploadthing/react";
+import { useDropzone } from "@uploadthing/react/hooks";
+import { generateClientDropzoneAccept } from "uploadthing/client";
+
+import { useUploadThing } from "~/utils/uploadthing";
+
+export function MultiUploader() {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+
+  const { startUpload, permittedFileInfo } = useUploadThing(
+    "myUploadEndpoint",
+    {
+      onClientUploadComplete: () => {
+        alert("uploaded successfully!");
+      },
+      onUploadError: () => {
+        alert("error occurred while uploading");
+      },
+      onUploadBegin: () => {
+        alert("upload has begun");
+      },
+    }
+  );
+
+  const fileTypes = permittedFileInfo?.config
+    ? Object.keys(permittedFileInfo?.config)
+    : [];
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: fileTypes ? generateClientDropzoneAccept(fileTypes) : undefined,
+  });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <div>
+        {files.length > 0 && (
+          <button onClick={() => startUpload(files)}>
+            Upload {files.length} files
+          </button>
+        )}
+      </div>
+      Drop files here!
+    </div>
+  );
+}
 
 const FileUploader = (imageUrl, onFieldChange, setFiles) => {
   return <div>FileUploader</div>;
