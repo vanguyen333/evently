4053e24edc06119557f072d6c6ac54aa14416acb
vanguyen333 <vanguyen333@yahoo.com>
2024-01-04 13:56:18 -0500

    added basic try catch

--- lib/actions/category.actions.ts
+++ lib/actions/category.actions.ts
@@ -1,7 +1,20 @@
 "use server";
 
 import { CreateCategoryParams } from "@/types";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import Category from "../database/models/category.model";
 
 export const createCategory = async ({
   categoryName,
-}: CreateCategoryParams) => {};
+}: CreateCategoryParams) => {
+  try {
+    await connectToDatabase();
+
+    const newCategory = await Category.create({ name: categoryName });
+
+    return JSON.parse(JSON.stringify(newCategory));
+  } catch (error) {
+    handleError(error);
+  }
+};
